39c2e813001967ec4caf3117d652ac2be13bb5f2
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-10 18:00:19 -0600

    Wrote data access methods based on current migrations/seeds.

--- api/cookbook/cookbook-model.js
+++ api/cookbook/cookbook-model.js
@@ -1,4 +1,21 @@
+const db = require('../../data/db-config');
 
 module.exports = {
-
+    getRecipes() {
+        return db('recipes r')
+            .join('ingredients i', 'ingredient_id', 'r.ingredient')
+            .select('r.recipe_id', 'r.step_id', 'i.ingredient_id');
+    },
+    getShoppingList(recipe_id) {
+        return db('ingredients i')
+            .join('recipes r', 'i.ingredient_id', 'r.recipe_id')
+            .join('i.ingredient_name', 'r.recipe_id', 'r.quantity')
+            .select('i.ingredient_name', 'r.quantity')
+            .where('r.recipe_id', recipe_id);
+    },
+    getInstructions(recipe_id) {
+        return db('recipes r')
+            .where('r.recipe_id', recipe_id)
+            .orderBy('r.step_id', 'asc');
+    },
 };
